d4561f2ec1214828eba55f1a429341c9e70aa35c
Ray Lee <fsdp0dev@gmail.com>
2024-12-06 16:51:33 +0900

    [FSDP-FB-0003] Automation Commit

--- src/admin/util/firebase.admin.util.ts
+++ src/admin/util/firebase.admin.util.ts
@@ -1,41 +1,39 @@
-// import type { ConfigService } from "@nestjs/config";
+import type { ConfigService } from "@nestjs/config";
 
-// import { applicationDefault, cert, initializeApp } from "firebase-admin/app";
-// import type { Credential, ServiceAccount } from "firebase-admin/app";
+import { applicationDefault, cert, initializeApp } from "firebase-admin/app";
+import type { Credential, ServiceAccount } from "firebase-admin/app";
 
-// import { FIREBASE_SDK_ADMIN } from "../constant/firebase.admin.constant";
-// import { isObjectEmpty } from "@fsdp0/common";
+import { FIREBASE_SDK_ADMIN } from "../constant/firebase.admin.constant";
+import { isObjectEmpty } from "@fsdp0/common";
 
-// const {
-//   CONFIG: { NAMESPACE }
-// } = FIREBASE_SDK_ADMIN;
+const {
+  CONFIG: { NAMESPACE }
+} = FIREBASE_SDK_ADMIN;
 
-// export function firebaseSdkAdminInitialize(configService: ConfigService) {
-//   let credential: Credential;
+export function firebaseSdkAdminInitialize(configService: ConfigService) {
+  let credential: Credential;
 
-//   if (process.env["GOOGLE_APPLICATION_CREDENTIALS"]) {
-//     credential = applicationDefault();
-//   } else {
-//     const serviceAccountCredentialFilePath = configService.get<string>(
-//       "FIREBASE_SVC_CREDENTIAL_FILE_PATH"
-//     );
+  if (process.env["GOOGLE_APPLICATION_CREDENTIALS"]) {
+    credential = applicationDefault();
+  } else {
+    const serviceAccountCredentialFilePath = configService.get<string>(
+      "FIREBASE_SVC_CREDENTIAL_FILE_PATH"
+    );
 
-//     if (serviceAccountCredentialFilePath) {
-//       credential = cert(serviceAccountCredentialFilePath);
-//     } else {
-//       const serviceAccount = configService.get<ServiceAccount>(
-//         NAMESPACE.CREDENTIAL.SERVICE_ACCOUNT
-//       );
+    if (serviceAccountCredentialFilePath) {
+      credential = cert(serviceAccountCredentialFilePath);
+    } else {
+      const serviceAccount = configService.get<ServiceAccount>(
+        NAMESPACE.CREDENTIAL.SERVICE_ACCOUNT
+      );
 
-//       if (serviceAccount && !isObjectEmpty(serviceAccount as object)) {
-//         credential = cert(serviceAccount);
-//       } else {
-//         throw Error("Error occured when initalize firebase service");
-//       }
-//     }
-//   }
+      if (serviceAccount && !isObjectEmpty(serviceAccount as object)) {
+        credential = cert(serviceAccount);
+      } else {
+        throw Error("Error occured when initalize firebase service");
+      }
+    }
+  }
 
-//   initializeApp({ credential });
-// }
-
-export function dummy() {}
+  initializeApp({ credential });
+}
