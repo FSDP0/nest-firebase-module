492267e1ba797dd8450afd7882cf9c236ae2b602
Ray Lee <fsdp0dev@gmail.com>
2024-12-06 16:52:23 +0900

    [FSDP-FB-0003] Automation Commit

--- src/admin/firebase.admin.module.ts
+++ src/admin/firebase.admin.module.ts
@@ -1,14 +1,8 @@
 import { Logger, Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
-import type { ServiceAccount } from "firebase-admin/app";
-
-import { FIREBASE_SDK_ADMIN } from "./constant";
 import { firebaseSdkAdminServiceAccount } from "./config";
-
-const {
-  CONFIG: { NAMESPACE }
-} = FIREBASE_SDK_ADMIN;
+import { firebaseSdkAdminInitialize } from "./util";
 
 @Module({
   imports: [ConfigModule.forFeature(firebaseSdkAdminServiceAccount)]
@@ -17,10 +11,6 @@ export class FirebaseAdminModule {
   private readonly logger = new Logger(FirebaseAdminModule.name);
 
   constructor(private readonly configService: ConfigService) {
-    this.configService.getOrThrow<ServiceAccount>(
-      NAMESPACE.CREDENTIAL.SERVICE_ACCOUNT,
-      {},
-      { infer: true }
-    );
+    firebaseSdkAdminInitialize(configService);
   }
 }
